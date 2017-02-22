3b717358859f4808accc0726856a20f0542127eb
Seth Figueroa <sethfigueroa@dhcp-v085-002.mobile.uci.edu>
2017-02-22 15:52:48 -0800

    Mac version will delete later

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 (let ((default-directory  "~/.emacs.d/"))
   (normal-top-level-add-subdirs-to-load-path))
 
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
 ;; Set theme and font
 (require 'tomorrow-night-bright-theme)
 (add-to-list 'default-frame-alist
