e1cb97b67cdaf14593020129d0587e0503598d53
whodatXLIV <sethamin.7@gmail.com>
2017-02-22 13:21:43 -0800

    Font and theme added in init, Theme folder added

--- init.el
+++ init.el
@@ -1,1 +1,11 @@
-(load-theme 'adwaita)
+;; no startup msg  
+(setq inhibit-startup-message t)        ; Disable startup message
+
+;; Add my .emacs to path as well as all its subfolders
+(let ((default-directory  "~/.emacs.d/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; Set theme and font
+(require 'tomorrow-night-bright-theme)
+(add-to-list 'default-frame-alist
+             '(font . "consolas-10:bold"))
