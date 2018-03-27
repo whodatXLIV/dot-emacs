5d592c3b2888c336fb2b196d0c479eef937e97b3
Seth_PC <sethamin.7@gmail.com>
2018-03-27 15:45:48 -0700

    Added org capture, working on windows

--- config/config-org.el
+++ config/config-org.el
@@ -3,9 +3,23 @@
   (global-set-key "\C-ca" 'org-agenda)
   (global-set-key "\C-cc" 'org-capture)
   (global-set-key "\C-cb" 'org-switchb)
+  
   (setq org-todo-keywords
 	'((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELLED")))
-  (setq org-agenda-files '( "~/Google\ Drive/Org/"))
+
+  (setq org-directory '("~/Google\ Drive/Org"))  
+  (setq org-agenda-files org-directory)
+  (setq org-archive-location '("~/Google Drive/Org~/Google\ Drive/Org/Archive"))
+  (setq org-default-notes-file '("~/Google Drive/Org/notes.org"))
+
+  (setq org-capture-templates
+	'(("t" "Todo" entry (file+headline "~/Google Drive/Org/notes.org" "Tasks")
+	   "* TODO %?\n  %i\n  %a")))
+  
+  (setq org-startup-folded t)
+  (setq org-startup-indented t)
+  (setq org-log-done t)
+  
   )
 
 (provide 'config-org)
