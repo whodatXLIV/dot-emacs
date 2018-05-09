fa82f823d0989cc853f8b8a6960403d113a51156
whodatXLIV <sethamin.7@gmail.com>
2018-05-08 23:53:08 -0700

    adding branch for config-ein

---
+++ config/config-EIN.el
@@ -0,0 +1,15 @@
+;;; package --- EIN configuration
+
+;;; Commentary:
+
+;;; Code:
+(require-package 'ein)
+(require 'ein)
+(require 'ein-loaddefs)
+(require 'ein-notebook)
+(require 'ein-subpackages)
+
+;; (global-set-key (kbd "") 'ein:jupyter-server-start)
+
+(provide 'config-ein)
+;;; config-EIN.el ends here
--- config/config-magit.el
+++ config/config-magit.el
@@ -1,6 +1,12 @@
+;;; package --- magit configuration
+
+;;; Commentary:
+
+;;; Code:
 (require-package 'magit)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (setenv "GIT_ASKPASS" "git-gui--askpass")
 
 (provide 'config-magit)
+;;; config-magit.el ends here
