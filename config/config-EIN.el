13833bf7576de3f2c1deaf8157ce27c898c860fa
Seth_PC <sethamin.7@gmail.com>
2018-05-09 11:29:39 -0700

    EIN working with windows

--- config/config-EIN.el
+++ config/config-EIN.el
@@ -3,13 +3,18 @@
 ;;; Commentary:
 
 ;;; Code:
-(require-package 'ein)
-(require 'ein)
-(require 'ein-loaddefs)
-(require 'ein-notebook)
-(require 'ein-subpackages)
 
-;; (global-set-key (kbd "") 'ein:jupyter-server-start)
+;; pip install tornado
+(require-package 'ein)
+(require-package 'websocket)
+(require-package 'request)
+(require-package 'dash)
+(require-package 's)
+(require-package 'skewer-mode)
+
+(setq ein:jupyter-server-args (list "--no-browser"))
+
+(global-set-key (kbd "C-c C-j") 'ein:jupyter-server-start)
 
 (provide 'config-ein)
 ;;; config-EIN.el ends here
