110569c680779fc11b1b78153d11a19797f8284f
Seth_PC <sethamin.7@gmail.com>
2018-06-18 12:10:21 -0700

    Changed jump mode key binding

--- config/config-ace-jump-mode.el
+++ config/config-ace-jump-mode.el
@@ -5,7 +5,7 @@
 ;;; Code:
 (require-package 'ace-jump-mode)
 
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-c j") 'ace-jump-mode)
 
 (provide 'config-ace-jump-mode)
 ;;; config-ace-jump-mode.el ends here
