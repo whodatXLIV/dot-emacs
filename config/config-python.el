edef5508b4c949e70e59120deff9af7245906717
Seth_PC <sethamin.7@gmail.com>
2018-08-10 11:58:14 -0700

    Changed company-anaconda and anaconda-mode

--- config/config-python.el
+++ config/config-python.el
@@ -7,26 +7,22 @@
 
 (after 'python-mode
 
-  ;; (setq-default py-shell-name "ipython"
-  ;; 		py-ipython-command-args "--simple-prompt -i")
+  (require-package 'anaconda-mode)
+  (require-package 'company-anaconda)
+  (require-package 'py-yapf)
   
-  ;; (setq-default py-which-bufname "IPython")
-
 ; don't split windows
   (setq py-split-window-on-execute nil)
 ; try to automagically figure out indentation
   (setq py-smart-indentation t)
-  (ein:jupyter-server-start (executable-find "jupyter") (getenv '"HOME"))
-  (after "company-autoloads"
-;    (add-to-list 'company-backends 'company-anaconda)
-    (add-to-list 'company-backends 'company-anaconda))
-
+ (ein:jupyter-server-start (executable-find "jupyter") (getenv '"HOME"))
 
   (require 'anaconda-mode)
   (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-  
-  (require-package 'py-yapf)
+;;  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+  (after "company-autoloads"
+    (add-to-list 'company-backends 'company-anaconda))
+
   (require 'py-yapf)
   (add-hook 'python-mode-hook 'py-yapf-enable-on-save)
 
--- init.el
+++ init.el
@@ -110,8 +110,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-
-    (pdf-tools latex-mode LaTeX-mode go-dlv projectile go-playground minimap ace-window avy markdown-mode py-yapf flake8 company-jedi pylint company-anaconda python-mode undo-tree go-flymake swiper org-mode use-package smartparens py-autopep8 multiple-cursors magit latex-preview-pane js2-mode jedi golden-ratio flymd flycheck-pos-tip exec-path-from-shell emmet-mode elpy diminish counsel company-go auctex))))
+    (anaconda-mode pdf-tools latex-mode LaTeX-mode go-dlv projectile go-playground minimap ace-window avy markdown-mode py-yapf flake8 company-jedi pylint company-anaconda python-mode undo-tree go-flymake swiper org-mode use-package smartparens py-autopep8 multiple-cursors magit latex-preview-pane js2-mode jedi golden-ratio flymd flycheck-pos-tip exec-path-from-shell emmet-mode elpy diminish counsel company-go auctex))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
