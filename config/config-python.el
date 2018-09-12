39dae8b23b5d2d14ca07e65008b85781282b35ab
whodatXLIV <sethamin.7@gmail.com>
2018-09-12 14:39:58 -0700

    Configure jedi (#5)
    * configured autostart and python
    
    * Fixed and config jedi and auto complete
    
    * changed auto start to 2 in autocompletion
    
    * changed tabs to spaces

--- config/config-python.el
+++ config/config-python.el
@@ -5,19 +5,32 @@
 ;;; Code:
 (/boot/lazy-major-mode "\\.py$" python-mode)
 (after 'python-mode
- 
+
   (require-package 'jedi)
   (add-hook 'python-mode-hook 'jedi:setup)
-  (setq jedi:complete-on-dot t) 
+  (setq jedi:complete-on-dot t)
+  (setq jedi:tooltip-method nil)
+  (setq jedi:get-in-function-call-delay 1000000)
 
-  ;; ; don't split windows
+  (global-set-key [C-tab] 'auto-complete)
+  ;; ;; ; don't split windows
+  ;; (add-to-list 'ac-sources 'ac-source-jedi-direct)
+
   (setq py-split-window-on-execute nil)
 ; try to automagically figure out indentation
-  (setq py-smart-indentation t)
-  (ein:jupyter-server-start (executable-find "jupyter") (getenv '"HOME"))
-  
+  ;; (setq py-smart-indentation t)
+  (add-hook 'python-mode-hook
+	    (lambda ()
+		  (setq-default indent-tabs-mode nil)
+		  (setq python-indent-offset 4)
+		;;     (setq-default tab-width 4)
+		;;     (setq-default py-indent-tabs-mode t)
+	    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+
+  ;;(ein:jupyter-server-start (executable-find "jupyter") (getenv '"HOME"))
+
   (require-package 'anaconda-mode)
-  (require 'anaconda-mode)
+  (add-hook 'python-mode-hook 'anaconda-mode)
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
   (require 'py-yapf)
@@ -28,8 +41,9 @@
   ;; yapf 'pip -m install yapf' for style with py-yapf
   ;; need to be downloaded seperately and set paths
 
- )  
+ )
 
+;;ac-source-jedi-direct
 
 (provide 'config-python)
 ;;; config-python.el ends here
