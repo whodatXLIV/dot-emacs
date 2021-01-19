2e3b27c418bd403cf48f8e58288f001b845cbe7a
Seth Figueroa FAE <seth.figueroa@focusae.com>
2021-01-19 13:11:21 -0600

    tabbar changes

--- transient/history.el
+++ transient/history.el
@@ -1,17 +1,34 @@
 ((magit-branch nil)
  (magit-commit nil)
  (magit-diff
+  (("--" "ssDefs.py")
+   "--no-ext-diff" "--stat")
+  (("--" "deploy/GMPN.py")
+   "--no-ext-diff" "--stat")
   ("--no-ext-diff" "--stat")
   ("--no-ext-diff"))
  (magit-dispatch nil)
  (magit-fetch nil)
  (magit-gitignore nil)
  (magit-log
-  ("-n256" "--graph" "--decorate"))
+  ("-n256" "--graph" "--decorate")
+  ("-n256"
+   ("--" "fsim.py")
+   "--graph" "--decorate")
+  ("-n256"
+   ("--" "ssDefs.py")
+   "--graph" "--decorate")
+  ("-n256"
+   ("--" "factor/*")
+   "--graph" "--decorate")
+  ("-n256"
+   ("--" "deploy/*")
+   "--graph" "--decorate"))
  (magit-merge nil)
  (magit-pull nil)
  (magit-push nil)
  (magit-rebase nil)
  (magit-reset nil)
  (magit-revert nil
-               ("--edit")))
+               ("--edit"))
+ (magit:-- "fsim.py" "" "ssDefs.py" "factor/*" "deploy/*" "deploy/GMPN.py"))
