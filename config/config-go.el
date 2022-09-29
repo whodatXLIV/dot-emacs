49fcb801a9397d0c30e0a976e2a4136afdf85a25
Seth Figueroa FAE <seth.figueroa@focusae.com>
2022-09-29 22:13:49 +0100

    lsp

--- config/config-go.el
+++ config/config-go.el
@@ -15,27 +15,28 @@
 (after 'go-mode
 
   ;; EGLOT
-  ;; (add-hook 'go-mode-hook 'eglot-ensure)
   ;; (defun project-find-go-module (dir)
   ;;   (when-let ((root (locate-dominating-file dir "go.mod")))
   ;;     (cons 'go-module root)))
 
+  ;; (add-hook 'project-find-functions #'project-find-go-module)
+
   ;; (cl-defmethod project-root ((project (head go-module)))
   ;;   (cdr project))
 
-  ;; (add-hook 'project-find-functions #'project-find-go-module)
 
   ;; (defun eglot-format-buffer-on-save ()
   ;;   (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
   ;; (add-hook 'go-mode-hook #'eglot-format-buffer-on-save)
 
-  
+  ;; (add-hook 'go-mode-hook 'eglot-ensure)
+
   ;; Company mode
   (setq company-idle-delay 1)
   (setq company-minimum-prefix-length 1)
   (add-hook 'go-mode-hook 'company-mode)
   
-  ;; (setq flycheck-global-modes '(not go-mode))
+  (setq flycheck-global-modes '(not go-mode))
 
   ;; Go - lsp-mode
   ;; Set up before-save hooks to format buffer and add/delete imports.
@@ -50,57 +51,14 @@
 
   (require 'lsp-mode)
   (lsp-register-client
-   (make-lsp-client :new-connection (lsp-tramp-connection "gopls")
-                    :major-modes '(go-mode)
-                    :priority -1
-                    :remote? t
-                    )
+   (make-lsp-client :new-connection (lsp-tramp-connection "gopls")                                                          
+                  :major-modes '(go-mode)
+                  :priority 10
+                  :server-id 'gopls-remote
+                  :remote? t
+                  )
    )
 
-
-  ;; (add-hook 'go-mode-hook #'yas-minor-mode)
-
-
-
-  
-  ;; (setq gofmt-command "goimports")
-  ;; (add-hook 'before-save-hook 'gofmt-before-save)
-  ;; (add-hook 'go-mode-hook '(lambda ()
-  ;;   		     (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-  ;; (add-hook 'go-mode-hook '(lambda ()
-  ;;   		     (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
-  ;; (add-hook 'go-mode-hook '(lambda ()
-  ;;   		     (local-set-key (kbd "C-c C-k") 'godoc)))
-  
-  ;; ;; (after "company-autoloads"
-  ;; ;;   (require-package 'company-go)
-  ;; ;;   (require 'company-go)
-  ;; ;;   (add-hook 'go-mode-hook (lambda ()
-  ;; ;;                             (set (make-local-variable 'company-backends) '(company-go)))))
-
-  ;; (after "auto-complete-autoloads"
-  ;;   (require-package 'go-autocomplete)
-  ;;   (require 'go-autocomplete)
-  ;;   (defun auto-complete-for-go ()
-  ;;     (auto-complete-mode 1))
-  ;;   (add-hook 'go-mode-hook 'auto-complete-for-go)
-  ;;   )
-
-  ;; (require-package 'go-eldoc)
-  ;; (require 'go-eldoc)
-  ;;   (add-hook 'go-mode-hook 'go-eldoc-setup)
-  
-  ;; (require-package 'go-playground)
-  ;; (add-to-list 'load-path "~/Projects/src/github.com/dougm/goflymake")
-  ;; (add-to-list 'load-path "~/Projects/src/github.com/cweill/gotests")
-  
-  ;; (add-hook 'go-mode-hook
-  ;;         (lambda ()
-  ;;           (flycheck-mode -1)))
-  ;; (require 'go-flymake)
-  ;; (require 'go-flycheck)
-  ;; (require 'gotests)
-  ;; (require-package 'go-dlv)
   )
 
 
--- config/config-python.el
+++ config/config-python.el
@@ -1,12 +1,55 @@
 ;;; package --- Python Configuration
 
 ;;; Commentary:
+;; (require-package 'pyenv-mode)
+;; (require-package 'flycheck)
+;; (require-package 'anaconda-mode)
+;; (add-hook 'python-mode-hook 'anaconda-mode)
+;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+;; (add-hook 'python-mode-hook (lambda ()
+;;                               (setq anaconda-mode-localhost-address "localhost")
+;;                               )
+;;           )
 
+
+;; (setq python-shell-interpreter "python3")
+;; (add-hook 'python-mode-hook 'flycheck-mode)
+;; (add-hook 'python-mode-hook
+;; 	      (lambda ()
+;; 		    (setq-default indent-tabs-mode nil)
+;; 		    (setq python-indent-offset 4)
+;; 		    ;;     (setq-default tab-width 4)
+;; 		    ;;     (setq-default py-indent-tabs-mode t)
+;; 	        (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+
+
+(require-package 'lsp-mode)
+(require-package 'lsp-ui)
+;; (require-package 'lsp-pyright)
+
+Start LSP Mode
+(add-hook 'python-mode-hook #'lsp-deferred)
+(setq lsp-headerline-breadcrumb-enable nil)
+
+(use-package lsp-mode
+  :hook
+  ((python-mode . lsp)))
+
+(require 'lsp-mode)
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-tramp-connection "pylsp")                                                          
+                  :major-modes '(python-mode)
+                  :priority 10
+                  :server-id 'pylsp-remote
+                  :remote? t
+                  )
+ )
+
+(require-package 'yapfify)
+(add-hook 'python-mode-hook 'yapf-mode)
 ;;; Code:
 (/boot/lazy-major-mode "\\.py$" python-mode)
 (after 'python-mode
-  (setq python-shell-interpreter "python3")
-  (require-package 'pyenv-mode)
   ;; (require-package 'jedi)
   ;; (add-hook 'python-mode-hook 'jedi:setup)
   ;; (setq jedi:complete-on-dot t)
@@ -15,12 +58,12 @@
 
 
   ;; (global-set-key [C-tab] 'auto-complete)
-  (add-hook 'python-mode-hook
-            (lambda ()
-              (local-set-key (kbd "C-c C-c") 'python-shell-send-buffer)))
-  (add-hook 'python-mode-hook
-            (lambda ()
-              (local-set-key (kbd "C-c p") 'run-python)))
+  ;; (add-hook 'python-mode-hook
+  ;;           (lambda ()
+  ;;             (local-set-key (kbd "C-c C-c") 'python-shell-send-buffer)))
+  ;; (add-hook 'python-mode-hook
+  ;;           (lambda ()
+  ;;             (local-set-key (kbd "C-c p") 'run-python)))
 
   ;; (add-hook 'python-mode-hook
   ;;           (lambda ()
@@ -29,32 +72,18 @@
   ;; ;; ; don't split windows
   ;; (add-to-list 'ac-sources 'ac-source-jedi-direct)
 
-  (setq py-split-window-on-execute nil)
+  ;; (setq py-split-window-on-execute nil)
 ; try to automagically figure out indentation
   ;; (setq py-smart-indentation t)
-  (add-hook 'python-mode-hook
-	    (lambda ()
-		  (setq-default indent-tabs-mode nil)
-		  (setq python-indent-offset 4)
-		;;     (setq-default tab-width 4)
-		;;     (setq-default py-indent-tabs-mode t)
-	    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-
   ;;(ein:jupyter-server-start (executable-find "jupyter") (getenv '"HOME"))
-
-  (require-package 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-  (add-hook 'python-mode-hook (lambda ()
-                                (setq anaconda-mode-localhost-address "localhost")
-                                )
-            )
 
   ;; (require 'py-yapf)
   ;; (add-hook 'python-mode-hook 'py-yapf-enable-on-save)
 
-  (require-package 'yapfify)
-  (add-hook 'python-mode-hook 'yapf-mode)
+  
+
+  
+
 
   ;; Flake8 'python -m pip install flake8' for flycheck
   ;; yapf 'pip -m install yapf' for style with py-yapf
